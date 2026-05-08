e49ff51486f3d8e761374da68d1f7382780fe537
Ivan Gerasimenko <66555504+evangera@users.noreply.github.com>
2026-05-08 14:22:17 +0300

    Create reverse_words.ts

---
+++ reverse_words.ts
@@ -0,0 +1,4 @@
+function reverseWords(str: string): string {
+  return str.split(" ").map(word => word.split("").reverse().join("")).join(" ")
+}
+console.log(reverseWords("The quick brown fox jumps over the lazy dog.")
